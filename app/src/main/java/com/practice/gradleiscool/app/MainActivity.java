11a72b6e2a3a4f91e48136f0e89865ec80e3a886
Habib Okanla <habib.okanla@gmail.com>
2014-05-16 09:59:19 -0400

    Initial commit

---
+++ app/src/main/java/com/practice/gradleiscool/app/MainActivity.java
@@ -0,0 +1,58 @@
+package com.practice.gradleiscool.app;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView view = (TextView) findViewById(R.id.text);
+
+        InputStream inputStream = getResources().openRawResource(R.raw.test);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder buffer = new StringBuilder();
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line);
+            }
+            view.setText(buffer.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
